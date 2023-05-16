6d720f87c2286def92c7e43eb3a2b876fca160d6
Soizic MARTIN <soizic.martin@live.fr>
2023-05-16 10:38:08 +0200

    Nous avons décidé de quitter le programme lors d'une erreure fatale

--- DB.py
+++ DB.py
@@ -3,7 +3,7 @@ from sys import argv
 from psycopg2.errors import *
 if len(argv) < 3 :
     print ("Nous ne pouvons donner suite à votre demande : nombre insuffisant d'argument donné. Nous avons besoin au minimum d'une colonne et d'une table.")
-    exit (0)
+    exit (1)
 
 connection = psycopg2.connect(port=5432, host="localhost", user="postgres", password="RogerRafa1523", dbname="DB Goldenline") 
 
@@ -23,6 +23,7 @@ try:
     cursor.execute(request)
 except UndefinedColumn : 
     print("le nom de colonne est erroné")
+    exit(1)
 
 result = cursor.fetchall()
 print(table.upper())    
