d4e123ce2e311391c2bb7c28074636fe591bb2e4
Soizic MARTIN <soizic.martin@live.fr>
2023-05-16 11:36:53 +0200

    Nous avons factorisé le code principal

--- DB.py
+++ DB.py
@@ -1,35 +1,14 @@
-import psycopg2
+import credentials
+
+from db_utils import *
 from sys import argv
-from psycopg2.errors import *
+
 if len(argv) < 3 :
     print ("Nous ne pouvons donner suite à votre demande : nombre insuffisant d'argument donné. Nous avons besoin au minimum d'une colonne et d'une table.")
     exit (1)
 
-connection = psycopg2.connect(port=5432, host="localhost", user="postgres", password="RogerRafa1523", dbname="DB Goldenline") 
+cursor = connectDB(user=credentials.USER, password=credentials.PASSWORD, dbname=credentials.DBNAME) 
+colonnes, table = getRequestInfo(argv)
+result = executeRequest(table, colonnes, cursor)
 
-cursor = connection.cursor()
-
-colonnes =argv[1]
-table = argv[-1]
-
-for i in range(2, len(argv)-1):
-    colonnes +=f", {argv[i]}"
-
-print(colonnes)
-
-request = f"SELECT {colonnes} FROM \"{table}\""
-
-try:
-    cursor.execute(request)
-except UndefinedColumn : 
-    print("le nom de colonne est erroné")
-    exit(1)
-
-result = cursor.fetchall()
-print(table.upper())    
-print(colonnes.upper().replace(",", "\t"))
-
-for line in result : 
-    for colonne in line : 
-        print(f"{colonne}\t", end="")
-    print("\n")
+displayResult(result, colonnes, table)
